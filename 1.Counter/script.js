549e0a805051330a77d92af76f7d5fad1e161232
Jitendra <nitianjitendra@gmail.com>
2026-08-19 19:21:58 +0530

    Improved Counter Logic

---
+++ 1.Counter/script.js
@@ -0,0 +1,28 @@
+let increment = document.querySelector("#increment");
+let decrement = document.querySelector("#decrement");
+let counter_value = document.querySelector("#counter-value");
+let changeBy = document.querySelector("#changeBy #count");
+let reset = document.querySelector("#reset");
+
+let changeBy_num = 1;
+
+let counter_num = Number(counter_value.textContent);
+
+increment.addEventListener("click", ()=>{
+    changeBy_num = Number(changeBy.valueAsNumber);
+    counter_num = counter_num + changeBy_num;
+    counter_value.textContent = counter_num;
+    
+})
+
+decrement.addEventListener("click", ()=>{
+    changeBy_num = Number(changeBy.valueAsNumber);
+    counter_num = counter_num - changeBy_num;
+    counter_value.textContent = counter_num;
+    
+})
+
+reset.addEventListener("click", ()=>{
+    counter_value.textContent = 0;
+    counter_num = 0;
+})
